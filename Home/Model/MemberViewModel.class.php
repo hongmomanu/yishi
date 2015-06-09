4cf6592db9235b05111bb465cfb07553f4a55959
jack <hongmomanu@163.com>
2015-06-09 14:25:20 +0800

    jack commit

--- Home/Model/MemberViewModel.class.php
+++ Home/Model/MemberViewModel.class.php
@@ -8,7 +8,7 @@ namespace Home\Model;
 use Think\Model\ViewModel;
 class MemberViewModel extends ViewModel{
 	public $viewFields = array(
-			'Member'=>array('id','uname','phone','group1','group2','group3','sex','status','company','posttime','expeirtime'),
+			'Member'=>array('id','uname','truename','phone','group1','group2','group3','sex','status','company','posttime','expeirtime'),
 			'Auth_group_access'=>array('uid','group_id','_on'=>'Auth_group_access.uid=Member.id')
 			);
 }
