f57f63ee2988d4d65b73dc6dca800b47df365eb0
jack <hongmomanu@163.com>
2015-06-11 11:31:41 +0800

    jack commit

--- Runtime/Cache/Home/815680b2f298099a91a6c59766882d1c.php
+++ Runtime/Cache/Home/815680b2f298099a91a6c59766882d1c.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>医师协会1</title>
+<title>医师协会</title>
 <link href="/Public/Home/css/style.css" type="text/css" rel="stylesheet" />
 <script type="text/javascript" src="/Public/Js/jquery-1.8.3.min.js"></script>
     <script type="text/javascript" src="/Public/Js/jquery.Xslider.js"></script>
